31eb320a158a10f5ea415514c92dc8bd28cc3c5f
mbrosseau <mbrosseau@salesforce.com>
2021-10-20 10:47:28 -0700

    updates

---
+++ force-app/main/default/aura/registerGame/registerGameController.js
@@ -0,0 +1,27 @@
+({
+    init : function (component) {
+        // Find the component whose aura:id is "flowData"
+        var flow = component.find("flowData");
+        
+
+        // In that component, start your flow. Reference the flow's API Name.
+        flow.startFlow("GameStartFlow" );
+    },
+    handleStatusChange : function (component, event) {
+        if(event.getParam("status") === "FINISHED") {
+            var outputVariables = event.getParam("outputVariables");
+            var outputVar;
+            for(var i = 0; i < outputVariables.length; i++) {
+               outputVar = outputVariables[i];
+               if(outputVar.name === "redirect") {
+                  var urlEvent = $A.get("e.force:navigateToSObject");
+                  urlEvent.setParams({
+                     "recordId": outputVar.value,
+                     "isredirect": "true"
+                  });
+                  urlEvent.fire();
+               }
+            }
+        }
+    }
+})
